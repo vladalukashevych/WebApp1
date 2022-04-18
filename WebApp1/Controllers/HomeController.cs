dcdfdead27bcb5c08e5bef7591c045de692abae7
Vlada Lukashevych <ipz211_lvv@student.ztu.edu.ua>
2022-04-18 17:16:53 +0200

    Created WebApp

---
+++ WebApp1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UsersID usersID)
+        {
+            return View("Hello", usersID);
+        }
+    }
+}
---
+++ WebApp1/Models/UsersID.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp1.Models
+{
+    public class UsersID
+    {
+        public string Name {get; set;}
+    }
+}
